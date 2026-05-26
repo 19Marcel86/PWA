893459b724ab418dd9ff8b0e6513db708816af10
19Marcel86 <136683817+19Marcel86@users.noreply.github.com>
2026-05-26 04:12:41 +0200

    Add files via upload

---
+++ lab1.php
@@ -0,0 +1,17 @@
+<?php
+$god = 20;
+$ukp_mj = $god * 12;
+$ukp_dana = $god * 365;
+echo "$god godina ima $ukp_mj mjeseci i $ukp_dana dana.";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Labos 1</title>
+</head>
+<body>
+</body>
+</html>
